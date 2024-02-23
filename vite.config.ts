d121eafd12fadc1e8e295cad6b605b0409dcb2cd
coding-jhuunn <139361270+coding-jhuunn@users.noreply.github.com>
2024-02-23 13:05:32 +0800

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deplyoying-vite-project-example",
+  base: "/myImageSlider/",
   plugins: [react()],
 });
